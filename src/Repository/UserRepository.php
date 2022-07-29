ca00f9926d856c12ec4909b4793c37068c0914f3
antoinekaboome <antoine01kaboome@gmail.com>
2022-07-29 17:26:53 +0000

    update

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -73,11 +73,11 @@ $is_deleted = true;
             ->executeQuery(
                 <<<'SQL'
             SELECT id,lastname,firstname,username,email,isvalid,is_kyc_check,account_type,title,fax,company_name,address,phone1,phone2,last_connect,title,fax,tva,is_deleted FROM public.user
-           
-            JSON_TEXT(roles) : role
+            WHERE roles::jsonb ?? :role
+             
             ORDER BY created_at desc
 SQL,
-['role' => $role]   
+                ['role' => $role]
             )->fetchAllAssociative();
     }
 
