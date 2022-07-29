54cd594994fed264f02f09945ed61247b0467eda
antoinekaboome <antoine01kaboome@gmail.com>
2022-07-29 17:18:40 +0000

    update

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -73,7 +73,7 @@ $is_deleted = true;
             ->executeQuery(
                 <<<'SQL'
             SELECT id,lastname,firstname,username,email,isvalid,is_kyc_check,account_type,title,fax,company_name,address,phone1,phone2,last_connect,title,fax,tva,is_deleted FROM public.user
-            WHERE roles::jsonb ?? :role
+            WHERE roles::json ?? :role
              
             ORDER BY created_at desc
 SQL,
