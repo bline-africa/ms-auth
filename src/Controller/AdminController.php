5b471b0c8a32a4e8d4d9717dcf883b9879eeb20f
antoinekaboome <antoine01kaboome@gmail.com>
2022-08-25 08:47:36 +0000

    update

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -127,7 +127,7 @@ class AdminController extends AbstractController
     {
         $req = $request->getContent();
         $id = json_decode($req)->id;
-        
+       $id =  Uuid::fromString($id);
         return $createUserService->confirmDeleteRequest($id);
     }
 
