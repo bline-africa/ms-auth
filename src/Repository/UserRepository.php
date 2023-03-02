539561812de6e9706590602728d13468b87d1fde
antoine kaboome <antoine01kaboome@gmail.com>
2023-03-02 16:56:30 +0000

    update

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -38,7 +38,7 @@ private $tokenStorageInterface;
         $entityManager = $this->getEntityManager();
 
        // $ltoken = new JWTUserToken([]);
-       dd(apache_request_headers());
+       dd($_SERVER);
 $tokenString = str_replace('Bearer ', '', apache_request_headers()['Authorization']);
 //$ltoken->setToken($tokenString);
 //$payload = $this->jwt->decode($tokenString);
