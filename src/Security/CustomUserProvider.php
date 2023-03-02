e821ad0c56c594a4967d8e78f8f436d196c803fa
antoine kaboome <antoine01kaboome@gmail.com>
2023-03-02 14:06:34 +0000

    update

--- src/Security/CustomUserProvider.php
+++ src/Security/CustomUserProvider.php
@@ -1,26 +1,70 @@
 <?php
 namespace App\Security;
 
-use Symfony\Bridge\Doctrine\Security\User\EntityUserProvider;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
-class CustomUserProvider extends EntityUserProvider
+class CustomUserProvider implements UserProviderInterface, PasswordUpgraderInterface
 {
-    public function loadUserByUsername($username)
+    /**
+     * Symfony calls this method if you use features like switch_user
+     * or remember_me. If you're not using these features, you do not
+     * need to implement this method.
+     *
+     * @throws UserNotFoundException if the user is not found
+     */
+    public function loadUserByIdentifier(string $identifier): UserInterface
     {
-        // Retrieve the user from the database based on the provided username
-        $user = $this->repository->findOneBy(['email' => $username]);
+        // Load a User object from your data source or throw UserNotFoundException.
+        // The $identifier argument is whatever value is being returned by the
+        // getUserIdentifier() method in your User class.
+        throw new \Exception('TODO: fill in loadUserByIdentifier() inside '.__FILE__);
+    }
 
-        if (!$user) {
-            throw new UsernameNotFoundException(sprintf('Email "%s" does not exist.', $username));
+    /**
+     * Refreshes the user after being reloaded from the session.
+     *
+     * When a user is logged in, at the beginning of each request, the
+     * User object is loaded from the session and then this method is
+     * called. Your job is to make sure the user's data is still fresh by,
+     * for example, re-querying for fresh User data.
+     *
+     * If your firewall is "stateless: true" (for a pure API), this
+     * method is not called.
+     *
+     * @return UserInterface
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Invalid user class "%s".', get_class($user)));
         }
 
-        // Add the user's roles to the user object
-        $roles = $user->getRoles();
-        $user->setRoles($roles);
+        // Return a User object after making sure its data is "fresh".
+        // Or throw a UserNotFoundException if the user no longer exists.
+        throw new \Exception('TODO: fill in refreshUser() inside '.__FILE__);
+    }
 
-        return $user;
+    /**
+     * Tells Symfony to use this provider for this User class.
+     */
+    public function supportsClass(string $class)
+    {
+        return User::class === $class || is_subclass_of($class, User::class);
+    }
+
+    /**
+     * Upgrades the hashed password of a user, typically for using a better hash algorithm.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        // TODO: when hashed passwords are in use, this method should:
+        // 1. persist the new password in the user storage
+        // 2. update the $user object with $user->setPassword($newHashedPassword);
     }
 }
 
